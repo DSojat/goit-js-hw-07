00f0ac3fed4972712d6c87d6c6afda0dff5c35b3
DSojat <145054801+DSojat@users.noreply.github.com>
2024-02-02 15:32:59 +0200

    hw ready

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,46 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function createBoxes(amount) {
+  const markup = [];
+  for (let index = 1; index <= amount; index++) {
+    const element = index;
+    let boxSize = 30;
+    boxSize += 10 * index - 10;
+    const rndColor = getRandomHexColor();
+    markup.push(
+      `<div class='colorbox' style='background:${rndColor}; width:${boxSize}px; height:${boxSize}px'></div>`
+    );
+  }
+  divBoxes.insertAdjacentHTML('beforeend', markup.join(''));
+  controlPoint.firstElementChild.value = '';
+}
+
+function destroyBoxes(quantityBoxes) {
+  const colorBoxes = document.querySelectorAll('.colorbox');
+  controlPoint.firstElementChild.value = '';
+  for (const element of colorBoxes) {
+    element.remove();
+  }
+}
+
+const divBoxes = document.querySelector('#boxes');
+const controlPoint = document.querySelector('#controls');
+
+controlPoint.addEventListener('click', event => {
+  const quantityBoxes = controlPoint.firstElementChild.value;
+  if (event.target.dataset.create === '') {
+    if (document.querySelectorAll('.colorbox')) {
+      destroyBoxes();
+    }
+    if (1 <= quantityBoxes && quantityBoxes <= 100) {
+      createBoxes(quantityBoxes);
+    } else {
+      alert('Your number set: min="1" & max="100"');
+    }
+  }
+  if (event.target.dataset.destroy === '') {
+    destroyBoxes();
+  }
+});
